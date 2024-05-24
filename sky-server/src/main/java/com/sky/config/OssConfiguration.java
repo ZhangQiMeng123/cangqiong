8fba47579cfc52a00a3f868ba34512c6d49ab0d9
user_zhang <276171557@qq.com>
2024-05-24 20:26:09 +0800

    菜品分页查询

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,28 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 阿里云工具类对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+    /**
+     * 生成阿里云工具
+     */
+    @Bean //项目启动后会自动调用该方法，创建该对象，交给spring容器管理
+    @ConditionalOnMissingBean //保证spring容器中只有一个该对象
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties){
+        log.info("开始创建阿里云文件上传工具类对象：{}",aliOssProperties);
+        return  new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
